37e8da05a59a37a3635bf43a48e01019b684ea7c
rg872 <gilang0293@gmail.com>
2018-05-24 01:35:03 +0700

    salah firebase setting

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import Firebase from 'firebase'
 
 const firebaseApp = Firebase.initializeApp({
   // Populate your firebase configuration data here.
-  apiKey: `AIzaSyCri5hsisHcf7q7_eIx9pCe52VMWT7eWqA`,
-  authDomain: `hacktiv-overflow-97477.firebaseapp.com`,
-  databaseURL: `https://hacktiv-overflow-97477.firebaseio.com`,
-  projectId: `hacktiv-overflow-97477`,
-  storageBucket: `hacktiv-overflow-97477.appspot.com`,
-  messagingSenderId: `820938653131`
+  apiKey: 'AIzaSyAOO3j0onKg-y-OlzxBa5Oz705VFefn8jw',
+    authDomain: 'hacktiv-overflow-55e9a.firebaseapp.com',
+    databaseURL: 'https://hacktiv-overflow-55e9a.firebaseio.com',
+    projectId: 'hacktiv-overflow-55e9a',
+    storageBucket: 'hacktiv-overflow-55e9a.appspot.com',
+    messagingSenderId: '178983103830'
 })
 
 export const firebase = firebaseApp
